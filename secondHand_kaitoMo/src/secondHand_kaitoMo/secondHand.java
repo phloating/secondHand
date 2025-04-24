bfc8b23f903326635e1cdcb7358de28955ee83de
1467768 <1467768@W49L203S02.sd61.bc.ca>
2025-04-24 10:28:33 -0700

    update testing

--- secondHand_kaitoMo/src/secondHand_kaitoMo/secondHand.java
+++ secondHand_kaitoMo/src/secondHand_kaitoMo/secondHand.java
@@ -5,6 +5,7 @@ public class secondHand {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stubs
 		System.out.println("testing");
+		System.out.println("testing number two");
 
 	}
 
