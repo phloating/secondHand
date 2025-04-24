e52283d4d9d2f20a5788b027958bc6d897bdb722
BrotherMo1 <155409498+BrotherMo1@users.noreply.github.com>
2025-04-24 10:07:11 -0700

    Update secondHand.java

--- secondHand_kaitoMo/src/secondHand_kaitoMo/secondHand.java
+++ secondHand_kaitoMo/src/secondHand_kaitoMo/secondHand.java
@@ -3,7 +3,7 @@ package secondHand_kaitoMo;
 public class secondHand {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stubsdasfa
+		// TODO Auto-generated method stubs
 
 	}
 
